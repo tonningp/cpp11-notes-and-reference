ac2ba4b5f194c634abdbc12c038b5b2dc4645cc1
Paul Tonning <vvcinstructor@gmail.com>
2018-11-18 08:34:20 -0800

    update

--- type_traits/main.cpp
+++ type_traits/main.cpp
@@ -2,10 +2,20 @@
  * http://www.cplusplus.com/reference/type_traits/
   Description:
 */
-#include<iostream>
-#include<type_traits>
 
-int main()
-{
-    return 0;
+#include <iostream>
+#include <type_traits>
+
+class A {};
+
+int main() {
+  std::cout << std::boolalpha;
+  std::cout << "is_object:" << std::endl;
+  std::cout << "int: " << std::is_object<int>::value << std::endl;
+  std::cout << "A: " << std::is_object<A>::value << std::endl;
+  std::cout << "A&: " << std::is_object<A&>::value << std::endl;
+  std::cout << "A*: " << std::is_object<A*>::value << std::endl;
+  std::cout << "int(int): " << std::is_object<int(int)>::value << std::endl;
+  std::cout << "int(*)(int): " << std::is_object<int(*)(int)>::value << std::endl;
+  return 0;
 }
